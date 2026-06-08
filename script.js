01aed804ed44c5d8d027763eeb005e5df0f4bcfe
JustBohdan41k <bohdan41k@hotmail.com>
2026-06-08 15:15:19 +0300

    - Added an adaptive navigation menu for mobile devices

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navUl = document.querySelector('ul');
 
+menuBtn.addEventListener('click', function() {
+    navUl.style.display = navUl.style.display === 'flex' ? 'none' : 'flex';
+});
